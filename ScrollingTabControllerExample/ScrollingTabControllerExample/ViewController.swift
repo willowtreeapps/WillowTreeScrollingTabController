401633e319ab7e4f6e46d7c753740fcb71627ae1
Erik LaManna <erik.lamanna@willowtreeapps.com>
2016-01-11 14:44:21 -0500

    Update copyright year

--- ScrollingTabControllerExample/ScrollingTabControllerExample/ViewController.swift
+++ ScrollingTabControllerExample/ScrollingTabControllerExample/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  ScrollingTabControllerExample
 //
-//  Copyright (c) 2015 WillowTree, Inc.
+//  Copyright (c) 2016 WillowTree, Inc.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
