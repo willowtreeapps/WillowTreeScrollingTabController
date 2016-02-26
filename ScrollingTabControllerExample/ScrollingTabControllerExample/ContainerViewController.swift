6cb8410e3ccee0bbe5e017584be0ec30d1b5b00a
Erik LaManna <erik.lamanna@willowtreeapps.com>
2016-02-25 21:06:28 -0500

    Bugfix to properly handle sizing tabs to fit width.

---
+++ ScrollingTabControllerExample/ScrollingTabControllerExample/ContainerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContainerViewController.swift
+//  ScrollingTabControllerExample
+//
+//  Created by Erik LaManna on 2/25/16.
+//  Copyright © 2016 WillowTree, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
